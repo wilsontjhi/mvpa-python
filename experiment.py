6f42a828522ce6e1efb245d8c5305d52af6c36c7
Wilson Tjhi <wtjhi@thoughtworks.com>
2019-02-14 23:23:22 +0800

    Add implementation using pysequential

--- experiment.py
+++ experiment.py
@@ -1,5 +1,10 @@
 from functools import reduce
 
+from datetime import datetime
+
+from dateutil import relativedelta
+from functional import seq
+
 
 def read_data():
     with open('fake.txt') as f:
@@ -10,15 +15,32 @@ def get_individual_heartrate(all_data):
     return (int(l) for l in all_data)
 
 
-if __name__ == '__main__':
-    result = \
-        reduce(
-            lambda acc, current: acc + current if current > 10 else acc,
-            reduce(
-                lambda acc, current: [acc[0] + 1,  *acc[1:]] if current > 0 else [0,  *acc[0:]],
-                map(lambda hr: 1 if hr > 5 else 0,  get_individual_heartrate(read_data())),
-                [0, 0]),
-            0
-        )
+def is_mvpa_minute(mvpa_threshold):
+    return lambda heart_rate: 1 if heart_rate > mvpa_threshold else 0
 
-    print(result)
+
+def get_result_with_py_functional():
+    return seq(get_individual_heartrate(read_data())) \
+        .map(lambda heart_rate: is_mvpa_minute(mvpa_threshold=5)(heart_rate)) \
+        .reduce(lambda acc, current: [acc[0] + 1,  *acc[1:]] if current > 0 else [0,  *acc[0:]], [0, 0]) \
+        .reduce(lambda acc, current: acc + current if current > 10 else acc, 0)
+
+
+def get_result_with_functools():
+    return reduce(
+        lambda acc, current: acc + current if current > 10 else acc,
+        reduce(
+            lambda acc, current: [acc[0] + 1,  *acc[1:]] if current > 0 else [0,  *acc[0:]],
+            map(is_mvpa_minute(mvpa_threshold=5),  get_individual_heartrate(read_data())),
+            [0, 0]),
+        0
+    )
+
+
+if __name__ == '__main__':
+    start = datetime.now()
+    result = get_result_with_py_functional()
+    end = datetime.now()
+    time_taken = relativedelta.relativedelta(end, start)
+    print('Result is ', result, ' with processing time ', time_taken)
+
