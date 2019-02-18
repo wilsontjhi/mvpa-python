19c9bde85e765925535cde5834732b0d9922237b
Wilson Tjhi <wtjhi@thoughtworks.com>
2019-02-18 15:49:17 +0800

    Add mutable logic for reduce

--- experiment.py
+++ experiment.py
@@ -15,6 +15,19 @@ def get_individual_heartrate(all_data):
     return (int(l) for l in all_data)
 
 
+def accumulate_immutable(acc, current):
+    return [acc[0] + 1,  *acc[1:]] if current > 0 else [0,  *acc[0:]]
+
+
+def accumulate_mutable(acc, current):
+    if current > 0:
+        acc[-1] = acc[-1] + 1
+        return acc
+    else:
+        acc.append(0)
+        return acc
+
+
 def is_mvpa_minute(mvpa_threshold):
     return lambda heart_rate: 1 if heart_rate > mvpa_threshold else 0
 
@@ -22,7 +35,7 @@ def is_mvpa_minute(mvpa_threshold):
 def get_result_with_py_functional():
     return seq(get_individual_heartrate(read_data())) \
         .map(lambda heart_rate: is_mvpa_minute(mvpa_threshold=119)(heart_rate)) \
-        .reduce(lambda acc, current: [acc[0] + 1,  *acc[1:]] if current > 0 else [0,  *acc[0:]], [0, 0]) \
+        .reduce(accumulate_mutable, [0, 0]) \
         .reduce(lambda acc, current: acc + current if current > 10 else acc, 0)
 
 
